2a2aab7b8e47079a5252abe95d410fc835739850
Marc <marcbensan.inq@gmail.com>
2025-04-06 14:55:44 -0400

    fix: new design for cards

--- src/components/cards.tsx
+++ src/components/cards.tsx
@@ -66,7 +66,7 @@ export function ExpandableCard({ leaders }: { leaders: Leader[] }) {
               <motion.div
                 layoutId={`card-${active.name}-${id}`}
                 ref={ref}
-                className="w-full max-w-[500px]  h-full md:h-fit md:max-h-[90%]  flex flex-col bg-white dark:bg-neutral-900 sm:rounded-3xl overflow-hidden"
+                className="w-full max-w-[500px] h-fit max-h-[90%]  flex flex-col bg-white dark:bg-neutral-900 overflow-hidden"
               >
                 <motion.div layoutId={`image-${active.name}-${id}`}>
                   <Image
@@ -75,7 +75,7 @@ export function ExpandableCard({ leaders }: { leaders: Leader[] }) {
                     height={200}
                     src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
                     alt={active.name}
-                    className="w-full h-80 lg:h-80 sm:rounded-tr-lg sm:rounded-tl-lg object-cover object-top"
+                    className="w-full h-80 lg:h-80 object-cover object-top"
                   />
                 </motion.div>
 
@@ -104,7 +104,7 @@ export function ExpandableCard({ leaders }: { leaders: Leader[] }) {
                   <div className="pt-4 relative px-4">
                     <motion.div
                       layout
-                      className="text-neutral-600 text-xs md:text-sm lg:text-base h-40 md:h-fit pb-10 flex flex-col items-start gap-4 overflow-auto dark:text-neutral-400 [mask:linear-gradient(to_bottom,white,white,transparent)] [scrollbar-width:none] [-ms-overflow-style:none] [-webkit-overflow-scrolling:touch]"
+                      className="text-neutral-600 text-sm lg:text-base h-40 h-fit pb-10 flex flex-col items-start gap-4 overflow-auto dark:text-neutral-400 [mask:linear-gradient(to_bottom,white,white,transparent)] [scrollbar-width:none] [-ms-overflow-style:none] [-webkit-overflow-scrolling:touch]"
                     >
                       {active.description}
                     </motion.div>
@@ -113,29 +113,46 @@ export function ExpandableCard({ leaders }: { leaders: Leader[] }) {
               </motion.div>
             </div>
           ) : null}
-          <ul className="max-w-2xl grid grid-cols-2 gap-4 mx-auto w-full md:gap-8">
-            {leaders.map((card) => (
+          <ul className="max-w-2xl space-y-4 mx-auto w-full">
+            {leaders.map((card, index) => (
               <motion.div
                 layoutId={`card-${card.name}-${id}`}
                 key={`card-${card.name}-${id}`}
                 onClick={() => setActive(card)}
-                className="p-4 flex flex-col border-1 md:flex-row justify-between bg-white shadow-sm items-center hover:bg-neutral-50 dark:hover:bg-neutral-800 rounded-xl cursor-pointer"
+                className="p-4 flex border-1 flex-row justify-between bg-white shadow-sm items-center hover:bg-neutral-50 dark:hover:bg-neutral-800 rounded-full cursor-pointer"
               >
-                <div className="flex gap-4 flex-col md:flex-row ">
-                  <motion.div layoutId={`image-${card.name}-${id}`}>
+                <div
+                  className={`flex gap-4 flex-row w-full ${
+                    index % 2 !== 0 && "justify-end"
+                  }`}
+                >
+                  <motion.div
+                    layoutId={`image-${card.name}-${id}`}
+                    className={`${
+                      index % 2 === 0 ? "order-first" : "order-last"
+                    }`}
+                  >
                     <Image
                       width={100}
                       height={100}
                       src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png"
                       alt={card.name}
-                      className="h-40 w-40 md:h-14 md:w-14 rounded-lg object-cover object-top"
+                      className={`h-14 w-14 rounded-full object-cover object-top`}
                     />
                   </motion.div>
-                  <div className="">
-                    <h3 className="font-medium text-neutral-800 dark:text-neutral-200 text-start md:text-left">
+                  <div
+                    className={`${
+                      index % 2 === 0 ? "order-last" : "order-first"
+                    }`}
+                  >
+                    <h3
+                      className={`font-medium text-neutral-800 dark:text-neutral-200  ${
+                        index % 2 !== 0 ? "text-right" : "text-left"
+                      }`}
+                    >
                       {card.name}
                     </h3>
-                    <p className="text-neutral-600 dark:text-neutral-400 text-start md:text-left">
+                    <p className="text-neutral-600 dark:text-neutral-400 text-start">
                       {card.day} - {card.time}
                     </p>
                   </div>
